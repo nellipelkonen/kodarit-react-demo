420dc4a41b57c46729864e61b3a0182b8424c29e
nellipelkonen <nell.pelkonen@edu.tampere.fi>
2022-08-23 10:36:17 +0300

    ads

--- newproject/src/pages/home.jsx
+++ newproject/src/pages/home.jsx
@@ -3,17 +3,7 @@ import React from 'react';
 export const Home = () => {
     return (
         <div className='maincontent'>
-            <h1>Home</h1>
-            <span>A simple tutorial for react components.</span>
-            
-            <div className='homelist'>
-                <ul>
-                    <li>react router</li>
-                    <li>usestate</li>
-                    <li>react router</li>
-                    <li>events</li>
-                </ul>
-            </div>
+            <h1>Hello World!</h1>
         </div>
     );
   }
